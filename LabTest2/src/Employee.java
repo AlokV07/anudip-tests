1c129ef228bc9ff85e90fb0d2bca7686f0426ccd
Alok Verma <119931645+AlokV07@users.noreply.github.com>
2022-12-15 16:39:15 +0530

    Add files via upload

---
+++ LabTest2/src/Employee.java
@@ -0,0 +1,15 @@
+
+public class Employee extends Member {
+    private String specialization;
+
+    // Constructor
+    
+    public Employee(String name, int age, String phoneNumber,
+                    String address, double salary, String specialization) {
+        
+    	// accessing data from member class
+    	
+    	super(name, age, phoneNumber, address, salary);
+        this.specialization = specialization;
+    }
+}
---
+++ LabTest2/src/Manager.java
@@ -0,0 +1,14 @@
+public class Manager extends Member{
+    private String department;
+
+    // Constructor
+    
+    public Manager(String name, int age, String phoneNumber,
+                    String address, double salary, String department) {
+        
+    	// accessing data from member class
+    	
+    	super(name, age, phoneNumber, address, salary);
+        this.department = department;
+    }
+}
